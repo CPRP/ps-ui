56d07449801f2a247f89e6e1f9c563666a56ca16
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-02-12 02:22:53 -0500

    updated

--- client/showImage.lua
+++ client/showImage.lua
@@ -1,4 +1,4 @@
-local function SetDisplay(bool, img) --added for moon-menu
+local function SetDisplay(bool, img)
     SetNuiFocus(bool, bool)
     SendNUIMessage({
         type = "showImage",
@@ -9,33 +9,11 @@ end
 
 local function ShowImage(img)
     SetDisplay(true, img)
-    TriggerEvent('moon-menu:client:menuopen')
 end
 
 RegisterNUICallback("showItemImage-callback", function(data, cb)
     SetDisplay(false)
     cb('ok')
-    TriggerEvent('moon-menu:client:menuclose')
 end)
 
 exports("ShowImage", ShowImage)
- 
--- local function SetDisplay(bool, img)
---     SetNuiFocus(bool, bool)
---     SendNUIMessage({
---         type = "showImage",
---         image = img,
---         status = bool,
---     })
--- end
-
--- local function ShowImage(img)
---     SetDisplay(true, img)
--- end
-
--- RegisterNUICallback("showItemImage-callback", function(data, cb)
---     SetDisplay(false)
---     cb('ok')
--- end)
-
--- exports("ShowImage", ShowImage)
